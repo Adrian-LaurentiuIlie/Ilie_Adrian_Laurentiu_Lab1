076a014b1e2d534f3fbcf88d119c92d7010c0e59
ilie.adrian1@outlook.com <ilie.adrian1@outlook.com>
2023-10-19 18:01:19 +0300

    Exercitiu

---
+++ Controllers/Tentativa.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ilie_Adrian_Laurentiu_Lab1.Controllers
+{
+    public class Tentativa : Controller
+    {
+        public String Index()
+        {
+            return "Bun venit utilizatorule";
+        }
+
+        public String Salut()
+        {
+            return "Salut domnule utilizator";
+        }
+
+        public String Mesaj()
+        {
+            String nume = "Adrian";
+            int varsta = 21;
+            return "Acesta este " + nume + " cu varsta de " + varsta; 
+        }
+    }
+}
